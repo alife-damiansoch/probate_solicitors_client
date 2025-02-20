8d96a759dffdf662d69cd582d7b4ef3ed969e508
Damian Socha <damian.socha@amigo.ie>
2025-02-20 16:01:38 +0000

    installed bootstrap js

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 import {Provider} from "react-redux";
 import store from './store/store.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
